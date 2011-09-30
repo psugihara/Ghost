5ecac189aebad50bfd906b3785567f873dd6c966
billy <billy.rennekamp@gmail.com>
2011-09-30 18:00:51 -0400

    better

--- app.js
+++ app.js
@@ -20,23 +20,31 @@ var game = new Game;
 everyone.now.board = game.board;
 */
 everyone.now.clients = [];
-var board = [];
+var deadBoard = [];
+var liveBoard = [];
 for(var i = 0; i < 16; i ++){
-  board[i] = [];
+  deadBoard[i] = [];
+  liveBoard[i] = [];
 }
 
-everyone.now.addToImage = function(name,cObj){
-  console.log(cObj);
+everyone.now.addToImage = function(cObj){
   var name = cObj.name;
   var x = cObj.x;
   var y = cObj.y;
-  if(board[x][y] === undefined){
-    board[x][y] = blue;
+  if(liveBoard[x] == undefined){
+      liveBoard[x][y] = name;
+      everyone.now.drawBoard(liveBoard);
   }
-  else if (board[x][y] == name){
+  else if(liveBoard[x][y] == undefined){
+    liveBoard[x][y] = name;
+    everyone.now.drawBoard(liveBoard);
+  }
+  else if (liveBoard[x][y] == name){
     //EVALUATE IF SOMEONE OCCUPIES THAT SPACE
-    
   }
+  else{
+  }
+  
   
   /*
   var pos = ArrayIndexOf(everyone.now.clients, function(obj){
@@ -45,36 +53,30 @@ everyone.now.addToImage = function(name,cObj){
   */
   //console.log("pos = "+pos);
   //console.log($.makeArray(everyone.now.clients[pos].draw).constructor.toString());
-  console.log(everyone.now.clients[pos].draw);
+  /*console.log(everyone.now.clients[pos].draw);
   everyone.now.clients[pos].draw = $.makeArray(everyone.now.clients[pos].draw);
   console.log(everyone.now.clients[pos].draw);
   everyone.now.clients[pos].draw.push(cObj);
   console.log(everyone.now.clients[pos].draw);
-  
+  */
 }
 
 everyone.now.setName = function(name){
-  console.log(everyone.now.clients);
   console.log(name);
   var pos = ArrayIndexOf(everyone.now.clients, function(obj){
       return obj.name == name;
   });
-  console.log(pos);
   if(pos > -1){
     this.now.askName("name is taken");
   }
   else{
-    console.log(name);
     var myObj = name;
     //var tmpArray = new Array([0,0],[1,1]);
     var myObj = {name:name,draw:[]};
     this.now.name = name;
     everyone.now.clients.push(myObj);
-    console.log(myObj.draw.constructor.toString());
-    console.log(everyone.now.clients[0].draw.constructor.toString());
-    console.log($.makeArray(everyone.now.clients[0].draw).constructor.toString());
-    console.log(everyone.now.clients);
     everyone.now.updateList();
+    everyone.now.drawBoard(liveBoard);
     console.log("Joined: " + name);
   }
 }
@@ -83,13 +85,10 @@ nowjs.on("connect", function() {
     
 });
 nowjs.on("disconnect", function() {
-  console.log(this.now.name);
   var left = this.now.name;
         var pos = ArrayIndexOf(everyone.now.clients, function(obj){
             return obj.name == left;
         })
-        console.log()
-        console.log(pos);
         if(pos >= 0){
             everyone.now.clients.splice(pos,1);
         }
@@ -105,6 +104,14 @@ everyone.now.distributeMessage = function(message) {
   return everyone.now.receiveMessage(this.now.name, message);
 };
 
+everyone.now.clearBoard = function(){
+  liveBoard = [];
+  for(var i = 0; i < 16; i ++){
+    liveBoard[i] = [];
+  }
+  everyone.now.drawBoard("clear");
+  
+}
 
 function ArrayIndexOf(a, fnc) {
     if (!fnc || typeof (fnc) != 'function') {

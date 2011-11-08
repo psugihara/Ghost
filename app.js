3ddd21f00be06785fb42b6f46b4ffbd94d375904
billy <billy.rennekamp@gmail.com>
2011-11-08 00:49:48 -0500

    step in the right direction

--- app.js
+++ app.js
@@ -27,124 +27,60 @@ everyone.now.GAMEHIGH = 30;
 
 // This is how you use the game...
 var game = new g.Game(everyone.now.GAMEWIDE, everyone.now.GAMEHIGH);
-var peter = new g.Player('Peter', '33');
-var bill = new g.Player('Billy', '22');
+var peter = new g.Player('Peter', '0');
+var bill = new g.Player('Billy', '1');
 // STONES WILL ONLY BE PLACED IF THEY MAKE A CONNECTED SHAPE!
 game.placeStones(peter, [[1,1], [1,3], [2,2], [0,2]]);
 g.printBoard(game.board)
-
-console.log(bill);
-/*
-var game = new g.Game(10, 10);
-var peter = game.addPlayer('peter', '33');
-var bill = game.addPlayer('bill', '22');
-game.placeStone(peter, 1, 1);
-game.placeStone(peter, 2, 0);
-game.placeStone(peter, 3, 0);
-game.placeStone(peter, 3, 2);
-game.placeStone(peter, 1, 2);
-game.placeStone(peter, 2, 3);
-game.placeStone(peter, 3, 1);
-console.log(peter.score);
-console.log(game.checkWinner());
-g.printBoard(game);
-*/
-
-everyone.now.distributeMessage = function(message){
-  everyone.now.receiveMessage(this.now.name, message);
-};
-/*
-var game = new Game;
-everyone.now.board = game.board;
-*/
+everyone.now.stableBoard = (game.board);
 everyone.now.clients = [];
-var deadBoard = [];
-var liveBoard = [];
-for(var i = 0; i < 16; i ++){
-  deadBoard[i] = [];
-  liveBoard[i] = [];
+
+
+
+everyone.now.distribute_draw = function(x,y){
+  	everyone.now.receive_draw(x, y);
 }
 
-everyone.now.addToImage = function(cObj){
+
+everyone.now.addToBoard = function(cObj){ 
+    
   console.log(cObj);
   var name = cObj.name;
-  var x = cObj.x;
-  var y = cObj.y;
-  
-  game.placeStone(bill, cObj.x, cObj.y);
-  g.printBoard(game);
-  //console.log(game.board);
+  var array = cObj.moveArray;
+  game.placeStones(peter, array);
+  g.printBoard(game.board);
+  everyone.now.stableBoard = game.board;
   everyone.now.drawBoard(game.board);
-  
-  /*
-  if(liveBoard[x] == undefined){
-      liveBoard[x][y] = name;
-      everyone.now.drawBoard(liveBoard);
-  }
-  else if(liveBoard[x][y] == undefined){
-    liveBoard[x][y] = name;
-    everyone.now.drawBoard(liveBoard);
-  }
-  else if (liveBoard[x][y] == name){
-    //EVALUATE IF SOMEONE OCCUPIES THAT SPACE
-  }
-  else{
-  }
-  */
-  
-  
-  /*
-  var pos = ArrayIndexOf(everyone.now.clients, function(obj){
-      return obj.name == name;
-  });
-  */
-  //console.log("pos = "+pos);
-  //console.log($.makeArray(everyone.now.clients[pos].draw).constructor.toString());
-  /*console.log(everyone.now.clients[pos].draw);
-  everyone.now.clients[pos].draw = $.makeArray(everyone.now.clients[pos].draw);
-  console.log(everyone.now.clients[pos].draw);
-  everyone.now.clients[pos].draw.push(cObj);
-  console.log(everyone.now.clients[pos].draw);
-  */
 }
+
+
+
+
+
+
 
 everyone.now.setName = function(name){
   console.log(name);
   var pos = ArrayIndexOf(everyone.now.clients, function(obj){
       return obj.name == name;
   });
+  
   if(pos > -1){
     this.now.askName("name is taken");
   }
   else{
-    var myObj = name;
-    //var tmpArray = new Array([0,0],[1,1]);
-    var myObj = {name:name,draw:[]};
-    this.now.name = name;
-    everyone.now.clients.push(myObj);
-    everyone.now.updateList();
-  
-  this.now.oldBoard = [];
- // console.log(everyone.now.GAMEWIDE);
-  for(i= 0;i<everyone.now.GAMEWIDE;i++){
-    this.now.oldBoard[i] = [];
-  //  console.log("i");
-    for(var j = 0; j < everyone.now.GAMEHIGH; j++){
-      this.now.oldBoard[i][j] = -1;
-    }
-  }
-
-  this.now.drawBoard(game.board);
-
-
-
+      this.now.drawBoard(game.board);
     console.log("Joined: " + name);
   }
 }
 
+
+
 nowjs.on("connect", function() {
     
 });
+
+
 nowjs.on("disconnect", function() {
   var left = this.now.name;
         var pos = ArrayIndexOf(everyone.now.clients, function(obj){
@@ -157,34 +93,11 @@ nowjs.on("disconnect", function() {
  everyone.now.updateList();
 });
 
-everyone.now.distribute_draw = function(x,y){
-  	everyone.now.receive_draw(x, y);
-}
-
-everyone.now.distributeMessage = function(message) {
-  return everyone.now.receiveMessage(this.now.name, message);
-};
 
 everyone.now.clearBoard = function(){
   game = new g.Game(everyone.now.GAMEWIDE, everyone.now.GAMEHIGH);
   peter = new g.Player('Peter', '33');
   bill = new g.Player('Billy', '22');
-  /*
-  everyone.now.oldBoard = [];
-  console.log(everyone.now.oldBoard);
-  for(i= 0;i<everyone.now.GAMEWIDE;i++){
-    everyone.now.oldBoard[i] = [];
-    console.log(everyone.now.oldBoard[i]);
-    for(var j = 0; j < everyone.now.GAMEHIGH; j++){
-      everyone.now.oldBoard[i][j] = -1;
-      console.log(everyone.now.oldBoard[i][j]);
-    }
-    console.log(everyone.now.oldBoard[i]);
-  }
-  console.log("cleared old board");
-  console.log(everyone.now.oldBoard);
-  console.log("cleared old board");
-  */
   everyone.now.drawBoard("clear");
   
 }

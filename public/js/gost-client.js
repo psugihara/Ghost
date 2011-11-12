ea2809d5949ced1f97abc12e4783b4da2053f524
Peter Sugihara <peter@campsh.com>
2011-11-12 00:10:37 -0500

    more little stylistic things

--- public/js/gost-client.js
+++ public/js/gost-client.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-  $("#clear-button").click(function(){
+$(document).ready(function() {
+  $("#clear-button").click(function() {
     now.clearBoard();
   });
 });
-now.updateList = function(){
+now.updateList = function() {
   var list = "LIST:<br>";
   for (var i in now.clients) {
     list += now.clients[i].name+"<br>";
@@ -19,39 +19,39 @@ var image = [];
 var draw = false;
 var r;
 
-now.ready(function(){
+now.ready(function() {
   nameTaken = true;
   //checkName("name?");
-  now.askName = function(string){
+  now.askName = function(string) {
     now.setName(prompt(string));
   }
   now.askName("Name?");
   start();
 });
 
-function start(){
+function start() {
   width = now.GAMEWIDE*now.GAMEUNIT;
   height = now.GAMEHIGH*now.GAMEUNIT;
   paper = Raphael(200,50,width,height);
   r = paper.rect(0,0,width,height).attr("fill","white").attr("stroke", "blue");
 
-  $(window).mousedown(function(){
+  $(window).mousedown(function() {
     draw = true;
   });
-  $(window).mouseup(function(){
+  $(window).mouseup(function() {
     draw = false;
     now.eraseTemp(now.currentDrawing);
     now.currentDrawing = [];
   });
 
   var lp = "";  
-  $(window).mousemove(function(e){
-    if (draw){
+  $(window).mousemove(function(e) {
+    if (draw) {
       var x = Math.floor(e.offsetX/now.GAMEUNIT);
       var y = Math.floor(e.offsetY/now.GAMEUNIT);
 
       var cp = x + "," + y;
-      if (lp != cp){
+      if (lp != cp) {
         lp = cp;
         var repeat = false;
 
@@ -64,7 +64,7 @@ function start(){
         });
         //  console.log(repeat);
 
-        if (repeat){
+        if (repeat) {
           console.log("closed drawing");
           now.addToBoard({name:now.name,moveArray:now.currentDrawing});
           // now.clearTemp(now.currentDrawing);
@@ -79,16 +79,15 @@ function start(){
     }
   });
 }
-tempDraw = function(array){
+tempDraw = function(array) {
   var x = array[0]*now.GAMEUNIT;
   var y = array[1]*now.GAMEUNIT;
   paper.rect(x,y,now.GAMEUNIT,now.GAMEUNIT).attr("fill","blue");;
-
 }
-now.eraseTemp = function(temps){
+now.eraseTemp = function(temps) {
   //console.log(temps);
   for (x in temps) {
-    if($.isArray(temps[x])){
+    if ($.isArray(temps[x])) {
       paper.rect((temps[x][0]*now.GAMEUNIT),(temps[x][1]*now.GAMEUNIT),now.GAMEUNIT,now.GAMEUNIT).attr("fill","white");
     }
   }  
@@ -99,16 +98,16 @@ now.lastStableBoard = [];
 colors = ["red","blue","green"];
 
 
-now.drawBoard = function(board){
+now.drawBoard = function(board) {
 
   var filler;
   //   console.log(board);
   console.log("drawBoard called");
   if (board == "clear") {
     now.lastStableBoard = [];
-    for(i= 0;i<now.GAMEWIDE;i++){
+    for (i= 0;i<now.GAMEWIDE;i++) {
       now.lastStableBoard[i] = [];
-      for(var j = 0; j < now.GAMEHIGH; j++){
+      for (var j = 0; j < now.GAMEHIGH; j++) {
         now.lastStableBoard[i][j] = -1;
       }
     }
@@ -119,13 +118,13 @@ now.drawBoard = function(board){
     r = paper.rect(0,0,width,height).attr("fill","white").attr("stroke", "blue");
     now.drawBoard(now.stableBoard);
   } else {
-    for(x in board){
-      for(y in board[x]){
+    for (x in board) {
+      for (y in board[x]) {
         filler = colors[board[x][y]];
-        if(!$.isArray(now.lastStableBoard[x])){
+        if (!$.isArray(now.lastStableBoard[x])) {
           paper.rect((x*now.GAMEUNIT),(y*now.GAMEUNIT),now.GAMEUNIT,now.GAMEUNIT).attr("fill",filler);
         } else {
-          if(board[x][y] != now.lastStableBoard[x][y] ){
+          if (board[x][y] != now.lastStableBoard[x][y] ) {
             paper.rect((x*now.GAMEUNIT),(y*now.GAMEUNIT),now.GAMEUNIT,now.GAMEUNIT).attr("fill",filler);
           }
         }
